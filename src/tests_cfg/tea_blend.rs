29f80b2f7d1f5600906724601f350014a7019fb4
Kirill Ivanov <bragov4ik@hotmail.com>
2025-10-14 13:25:11 +0300

    test

--- src/tests_cfg/tea_blend.rs
+++ src/tests_cfg/tea_blend.rs
@@ -3,23 +3,26 @@ use crate as sea_orm;
 use crate::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
-#[sea_orm(table_name = "lunch_set")]
+#[sea_orm(table_name = "tea_blend")]
 pub struct Model {
     #[sea_orm(primary_key)]
-    pub id: i32,
-    pub name: String,
     pub tea: Tea,
+    #[sea_orm(primary_key)]
+    pub blend_part_variety: String,
+    pub mass_grams: i32,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
-    #[sea_orm(has_many = "super::tea_blend::Entity")]
-    Tea,
+    #[sea_orm(belongs_to = "super::lunch_set::Entity",
+    from = "Column::Tea"
+    to = "super::lunch_set::Column::Tea")]
+    LunchSet,
 }
 
-impl Related<super::tea_blend::Entity> for Entity {
+impl Related<super::lunch_set::Entity> for Entity {
     fn to() -> RelationDef {
-        Relation::Tea.def()
+        Relation::LunchSet.def()
     }
 }
 
